da2a37b0f18ffb51b4949340ed9959238ff7f9b3
Cristian Ticu <cristian.ticu@eggnita.com>
2024-02-19 23:51:30 +0200

    First steps towards routing: imported react-router-dom dependency and made a test route

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 
 import Main from './Main'
 
+const router = createBrowserRouter([
+    {
+        path: '/weather',
+        element: <Main />,
+    },
+    {
+        path: '*',
+        element: <Navigate to='/weather' />
+    },
+]);
+
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <Main />
+    <RouterProvider router={router} />
 );
